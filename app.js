5471373746348ec5cdbfe75ea8dbc59dcc8cb8d7
okgosu <okgosu.exp@navercorp.com>
2017-04-16 14:04:47 +0900

    swagger api config
    config, swagger ui  setting

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ var app = require('express')();
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+  appRoot: __dirname, // required config
+
+  swaggerSecurityHandlers: {
+    api_key: function (req, authOrSecDef, scopesOrApiKey, cb) {
+      // your security code
+      if ('1234' === scopesOrApiKey) {
+        cb();
+      } else {
+        cb(new Error('access denied!'));
+      }
+    }
+  }
+
 };
+
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
+
+  // add swagger-ui (/docs)
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
 
   // install middleware
   swaggerExpress.register(app);

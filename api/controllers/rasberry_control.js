833e53d76c0881553f926124e901b48c4692cf3f
okgosu <okgosu.exp@navercorp.com>
2017-04-16 22:01:19 +0900

    LED control

--- api/controllers/rasberry_control.js
+++ api/controllers/rasberry_control.js
@@ -6,6 +6,22 @@ module.exports = function (name) {
     },
     setAquaLightModule: function(lightId, lightOperation) {
       console.log('====setAquaLightListModule====' + lightId + lightOperation);
+
+      var Gpio = require('onoff').Gpio;
+      var led = new Gpio(21, 'out');
+      var led_state = 0;
+      var iv = setInterval(function() {
+          led_state = led.readSync();
+          if(led_state == 0) led_state = 1;
+          else led_state =0;
+          led.writeSync(led_state)}, 200);
+
+      setTimeout(function() {
+        clearInterval(iv);
+        led.writeSync(0);
+        led.unexport();
+      }, 5000);
+
       var operationResult = {
         "deviceId":lightId,
         "deviceStatus":lightOperation,
